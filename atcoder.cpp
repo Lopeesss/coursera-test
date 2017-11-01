2d546b00d5b9e7e269bd8aeb95d94b629cd993e7
Danilo <danilosousalopes.com>
2017-11-01 10:14:19 -0200

    second

---
+++ atcoder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+int main () {
+    double media ;
+    
+    double a1, a2 ;
+    
+    cin >> a1 >> a2 ;
+    
+    
+    media = a2 + (a2-a1) ;
+
+    
+    cout << media<<endl ;
+}
